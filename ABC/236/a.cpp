38b976e693bfd293c97f0cee2e1357c756f2b007
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-05-12 16:44:14 +0900

    add ABC236a

---
+++ ABC/236/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    string s;
+    int a,b;
+    cin >> s >> a >> b; 
+    a--,b--;
+
+    char tmp;
+    tmp = s[a];
+    s[a] = s[b];
+    s[b] = tmp;
+    cout << s << endl;
+    return 0;
+}
