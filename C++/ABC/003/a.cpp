a21230a65a00e00659fdd819ca6f2cedf72a95a8
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2025-04-24 20:33:43 +0900

    AC

---
+++ C++/ABC/003/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x;
+	cin >> x;
+    int avelage_sarary = (1 + x) * 10000 /2;
+    cout << avelage_sarary << endl;
+    return 0;
+}
+
